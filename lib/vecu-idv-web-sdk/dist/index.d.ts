cc9d807f26393ab8cfbb3fb47d81537ee175c633
shyju.viswambaran <shyjuezy@gmail.com>
2025-07-17 14:47:50 -0400

    Remove deprecated Vecu IDV Web SDK distribution files, including type definitions, JavaScript, and source maps, to streamline the SDK and eliminate unused components.

--- lib/vecu-idv-web-sdk/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { VecuIDV } from './core/VecuIDV';
-export type { IVecuIDVConfig, IAPIConfig, Environment, Theme, LogLevel, IVerificationOptions, IUserData, IAddress, IVerificationSession, IVerificationResult, IDocumentData, ILivenessData, IFraudSignals, IFraudSignal, IProgress, IVerificationUI, VerificationStatus, VerificationDecision, IVerificationUIOptions, IVecuEvent, EventType, IEventHandler, IEventEmitter, IProgressEvent, IErrorEvent, IProvider, IProviderInitOptions, IProviderVerificationUI, IProviderWebhookResult, IProviderEvent, IProviderRegistry, IProviderLoader, IProviderConfig, ProviderName, ProviderFeature, IVecuError, IAPIResponse, ILogger, } from './types';
-export { VecuError, ConfigurationError, ValidationError, NetworkError, ProviderError, SessionError, TimeoutError, ErrorCodes, } from './utils/errors';
-export { SDK_VERSION, SDK_NAME, EVENTS, } from './constants';
-export { PROVIDER_FEATURES, SUPPORTED_PROVIDERS, } from './types/providers';
-export { Logger } from './utils/logger';
-export { EventEmitter } from './core/EventEmitter';
-//# sourceMappingURL=index.d.ts.map
